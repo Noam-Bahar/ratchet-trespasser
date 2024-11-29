00da4810f9ac1828ede82073a8e9d225de9b79da
Noam Bahar <noambahar2002@gmail.com>
2024-11-30 01:38:45 +0200

    Style: empty lines between hooks

--- src/hooks/use-previous.ts
+++ src/hooks/use-previous.ts
@@ -2,8 +2,10 @@ import { useEffect, useRef } from 'react';
 
 export default function usePrevious<T>(value: T) {
   const ref = useRef<T>();
+
   useEffect(() => {
     ref.current = value;
   });
+
   return ref.current;
 }
